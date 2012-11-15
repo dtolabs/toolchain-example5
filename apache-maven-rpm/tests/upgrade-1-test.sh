29b42300d7c2d3b7095bd018357f3643fc8a0966
Anthony Shortland <anthony@dtosolution.com>
2012-11-14 18:34:28 -0800

    Closes #5 by implementing the apache-maven-rpm:upgrade command to install/upgrade the RPM.

---
+++ apache-maven-rpm/tests/upgrade-1-test.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env roundup
+#
+# This file contains the test plan for the upgrade command.
+# Execute the plan by invoking: 
+#    
+#     rerun stubbs:test -m apache-maven-rpm -p upgrade
+#
+
+# The Plan
+# --------
+
+describe "upgrade"
+
+
+# ------------------------------
+# Replace this test. 
+it_fails_without_a_real_test() {
+    exit 1
+}
+# ------------------------------
+
