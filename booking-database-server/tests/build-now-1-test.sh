22daaba233b530af93393dc00de433fd933213a8
Anthony Shortland <anthony@dtosolution.com>
2012-11-16 21:09:10 -0800

    Closes #19 by adding jobs to build the modules as bin archives and integrating them with the toolchain-build-console deployment workflow

---
+++ booking-database-server/tests/build-now-1-test.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env roundup
+#
+# This file contains the test plan for the build-now command.
+# Execute the plan by invoking: 
+#    
+#     rerun stubbs:test -m booking-database-server -p build-now
+#
+
+# Helpers
+#
+[[ -f ./functions.sh ]] && . ./functions.sh
+
+# The Plan
+# --------
+
+describe "build-now"
+
+
+# ------------------------------
+# Replace this test. 
+it_fails_without_a_real_test() {
+    exit 1
+}
+# ------------------------------
+
---
+++ booking-database-server/tests/create-build-jobs-1-test.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env roundup
+#
+# This file contains the test plan for the create-build-jobs command.
+# Execute the plan by invoking: 
+#    
+#     rerun stubbs:test -m booking-database-server -p create-build-jobs
+#
+
+# Helpers
+#
+[[ -f ./functions.sh ]] && . ./functions.sh
+
+# The Plan
+# --------
+
+describe "create-build-jobs"
+
+
+# ------------------------------
+# Replace this test. 
+it_fails_without_a_real_test() {
+    exit 1
+}
+# ------------------------------
+
