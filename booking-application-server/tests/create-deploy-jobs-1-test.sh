79e5e7c3ea8c192f9edf545498b9322164601067
connaryscott <connaryscott@gmail.com>
2012-11-26 15:41:18 -0800

    rename command create-jobs to create-deploy-jobs in module booking-application-server to avoid ambibuity and standardization with respect to build and deploy implement create-deploy-jobs for the booking-database-server
    Fixes #26

--- booking-application-server/tests/create-deploy-jobs-1-test.sh
+++ booking-application-server/tests/create-deploy-jobs-1-test.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env roundup
 #
-# This file contains the test plan for the create-jobs command.
+# This file contains the test plan for the create-deploy-jobs command.
 # Execute the plan by invoking: 
 #    
-#     rerun stubbs:test -m booking-application-server -p create-jobs
+#     rerun stubbs:test -m booking-application-server -p create-deploy-jobs
 #
+
+# Helpers
+#
+[[ -f ./functions.sh ]] && . ./functions.sh
 
 # The Plan
 # --------
 
-describe "create-jobs"
+describe "create-deploy-jobs"
+
 
 if [ -f "${RERUN_MODULES}/booking-application-server/tests/functions.sh" ]
 then
@@ -20,8 +25,7 @@ it_runs_without_arguments() {
     # Make sure the deployment console is running.
     rerun toolchain-deploy-console:deploy
     # Create the Booking application server's jobs.
-    rerun booking-application-server:create-jobs
+    rerun booking-application-server:create-deploy-jobs
     # Remove
     rerun toolchain-deploy-console:remove
 }
-
